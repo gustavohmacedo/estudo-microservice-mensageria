15dbf1c87ef299025e92979d414f25ebbb2d2e1c
Macedo <gemc@gft.com>
2023-11-28 00:38:09 -0300

    Add microservice cliente

---
+++ msclients/src/main/kotlin/github/com/gustavohmacedo/msclients/MsclientsApplication.kt
@@ -0,0 +1,11 @@
+package github.com.gustavohmacedo.msclients
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsclientsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsclientsApplication>(*args)
+}
---
+++ msclients/src/test/kotlin/github/com/gustavohmacedo/msclients/MsclientsApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.com.gustavohmacedo.msclients
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsclientsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
